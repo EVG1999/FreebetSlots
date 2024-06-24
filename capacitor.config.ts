e268f100c5da89a17221f1465afdd3e3f0c89b36
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-06-24 16:06:28 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FreeBet',
+  webDir: 'dist'
+};
+
+export default config;
