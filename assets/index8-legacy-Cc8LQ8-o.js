525a3fec7d669386c56ea4ca3bbffb0657782c00
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-07-05 17:26:29 +0700

    Updates

--- assets/index8-legacy-Cc8LQ8-o.js
+++ assets/index8-legacy-Cc8LQ8-o.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-bdnHZ1JJ.js"],(function(o,e){"use strict";var t;return{setters:[o=>{t=o.a}],execute:function(){
+System.register(["./main-legacy-DlmshJ17.js"],(function(o,e){"use strict";var t;return{setters:[o=>{t=o.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-BjHaI06R.js
+++ assets/status-tap-legacy-BjHaI06R.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-bdnHZ1JJ.js","./index8-legacy-DTFPeJkw.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
+System.register(["./main-legacy-DlmshJ17.js","./index8-legacy-Cc8LQ8-o.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
