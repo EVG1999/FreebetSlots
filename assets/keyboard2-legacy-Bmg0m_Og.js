01584d161bbdb538b5659644666fe5437fe033bf
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-07-05 16:15:20 +0700

    Updates

--- assets/keyboard2-legacy-Bmg0m_Og.js
+++ assets/keyboard2-legacy-Bmg0m_Og.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-tQ9XpYxZ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./main-legacy-KI730-2r.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
