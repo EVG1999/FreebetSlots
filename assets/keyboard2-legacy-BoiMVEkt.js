aacb697e6940da7d0f2cf8c3b9efec4db73a3d3e
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-07-05 16:46:24 +0700

    Updates

--- assets/keyboard2-legacy-BoiMVEkt.js
+++ assets/keyboard2-legacy-BoiMVEkt.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-L51glwCA.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./main-legacy-DxsSAodm.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
