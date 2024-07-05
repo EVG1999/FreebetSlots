e0935221aceba2e7e1d0a63aa5f8440bfc5965f6
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-07-05 15:41:10 +0700

    Updates

--- assets/status-tap-legacy-DP4fXbbm.js
+++ assets/status-tap-legacy-DP4fXbbm.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-BzfvbKnC.js","./index8-legacy-CNio6My2.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
+System.register(["./main-legacy-UNBlFgEF.js","./index8-legacy-BdCymcIf.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
