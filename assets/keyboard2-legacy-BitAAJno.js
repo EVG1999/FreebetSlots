6221e092db554f1bebad07f0687addbef52d65d8
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-07-06 14:25:11 +0700

    Updates

--- assets/keyboard2-legacy-BitAAJno.js
+++ assets/keyboard2-legacy-BitAAJno.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-Ak3EG55r.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./main-legacy-DKEfWmhz.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-C2FXcTZp.js
+++ assets/status-tap-legacy-C2FXcTZp.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-Ak3EG55r.js","./index8-legacy-DAebH4qr.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
+System.register(["./main-legacy-DKEfWmhz.js","./index8-legacy-BIswp3bt.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
