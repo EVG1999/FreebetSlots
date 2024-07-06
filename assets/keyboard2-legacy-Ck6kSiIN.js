5c6ce32d7bbbf359834a3b4c2f029ea7a60a7f89
System Administrator <root@MacBook-Pro-Evgenij.local>
2024-07-06 16:15:12 +0700

    Updates

--- assets/keyboard2-legacy-Ck6kSiIN.js
+++ assets/keyboard2-legacy-Ck6kSiIN.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-Bp80auZW.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./main-legacy-BR9371pd.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-B7vYCShk.js
+++ assets/status-tap-legacy-B7vYCShk.js
@@ -1,4 +1,4 @@
-System.register(["./main-legacy-Bp80auZW.js","./index8-legacy-Davn8xL7.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
+System.register(["./main-legacy-BR9371pd.js","./index8-legacy-DqetnSo6.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
